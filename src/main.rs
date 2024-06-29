437de14780751b0d1412df357aac9755768d888a
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2024-06-28 20:39:27 -0600

    feat: Implement hay_treatment funtion for vowel words

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ fn vowel_check(word: &String) -> bool {
     word.to_lowercase().starts_with(&['a', 'e', 'i', 'o', 'u'])
 }
 
+fn hay_treatment(word: String) -> String {
+    let s = format!("{word}-hay");
+
+    s
+}
+
 fn main() {
     // wtt = word_to_test
     let wtt_vowel_function = String::from("Eat");
