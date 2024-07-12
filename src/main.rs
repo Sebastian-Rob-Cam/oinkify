c16490f11ede5400e7c10fcae6a2b98a46468203
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2024-07-11 21:05:39 -0600

    feat: Implement the ay_treatment function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+use std::collections::BTreeMap;
+
 fn vowel_check(word: &String) -> bool {
     word.to_lowercase().starts_with(&['a', 'e', 'i', 'o', 'u'])
+}
+
+fn ay_treatment(word: String) -> String {
+    let mut chars_btreemap: BTreeMap<i32, char> = BTreeMap::new();
+    let mut result_string: String = String::new();
+    let mut count: i32 = 0;
+
+    for c in word.chars() {
+        chars_btreemap.insert(count, c);
+        count += 1;
+    }
+
+    for (k, c) in &chars_btreemap {
+        if *k != 0 {
+            result_string.push(*c);
+        }
+    }
+
+    let first_consonant = chars_btreemap.get(&0).unwrap().to_string();
+    let final_string = format!("-{}ay", first_consonant);
+
+    result_string.push_str(&final_string);
+
+    result_string
 }
 
 fn hay_treatment(word: String) -> String {
